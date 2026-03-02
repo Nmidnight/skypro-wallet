c90acdf2c9f09fbda328e24cd075ac8f27a8396d
Nmidnight <bynanamid46@gmail.com>
2026-03-02 11:28:26 +0200

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-// Добавил импорт глобальных стилей
 import { GlobalStyles } from "./styles/GlobalStyles";
 import { AppRoutes } from "./routes/AppRoutes";
-import { AuthProvider } from "./context/AuthContext/AuthProvider";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-
+import { AuthProvider } from "./context/AuthContext/AuthProvider";
 function App() {
   return (
     <AuthProvider>
       <ToastContainer
-          position="top-right"
-          autoClose={3000}
+        position="top-right"
+        autoClose={3000}
       />
       <GlobalStyles />
       <AppRoutes />
